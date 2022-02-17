29c8373422bc0177b4cca2dfaae701c3480df58d
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-02-17 10:21:27 -0300

    criando menu mobile

---
+++ main.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
+}
