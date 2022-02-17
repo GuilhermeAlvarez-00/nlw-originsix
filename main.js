339ace42c179381a5a52635b0d6fe8c332af9a32
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-02-17 14:34:59 -0300

    colocando animação no menui mobile

--- main.js
+++ main.js
@@ -14,3 +14,14 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
