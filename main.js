3cf855d3dbb2cdb99fb9a21695f30bcee5a34e9f
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-02-19 17:13:44 -0300

    adicionando animações

--- main.js
+++ main.js
@@ -34,3 +34,19 @@ const swiper = new Swiper('.swiper', {
   mouseWhell: true,
   keyboard: true
 })
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`,
+  { interval: 100 }
+)
